cdcd621f77e2acf11989dfc3336665eddfa929f0
19102260_Ghozii <102580684+Ghozi19102260@users.noreply.github.com>
2023-11-05 01:16:41 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+website pendaftar beasiswa adalah sebuah website yang digunakan oleh seorang mahasiswa untuk mendaftarkan beasiswa baik beasiswa akademik atau beasiswa non-akademik. 
+sebelum mendaftar pastikan memiliki sebuah akun yang digunakan untuk melakukan proses pendaftaran beasiswa
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beasiswa</title>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm p-2">
+        <div class="container-fluid">
+            <h2>BEASISWA UNGGULAN</h2>
+        </div>
+    </nav>
+
+    <!-- Content -->
+    <div class="container d-felx justify-content-center align-items-center w-25">
+        <h3 class="text-center mt-5">PILIHAN BEASISWA</h3>
+        <div class="mt-4 d-flex justify-content-around align-items-center">
+            <a class="btn btn-secondary" href="app/auth/login.php">Akademik</a>
+            <a class="btn btn-secondary" href="app/auth/login.php">Non Akademik</a>
+        </div>
+    </div>
+
+</body>
+
+</html>
